74035962ab54086be6ff63e62e8f4fb2561f4656
ArezoMansouri <arezomansouri2000@gmail.com>
2022-12-19 15:21:29 +0430

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+//TODO: 1.3.2 Put your API key and CoinAPI's base url in the following constants
+const kAPIKey = '964209D8-DA45-4ABF-8E23-326882015DC4';
+const kBaseURL = 'https://rest.coinapi.io/v1/exchangerate/';
+//       /BTC/USD
